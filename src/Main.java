076570b721d1776cc1f4c3512fae0bd6e61b0f56
Kumar <alievkumarbek85@gmail.com>
2023-01-11 18:55:21 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Uchburchtuk uch = new Uchburchtuk();
+         uch.a= 12;
+         uch.b= 18;
+         uch.c= 16;
+         uch.method();
+    }
+}
---
+++ src/Uchburchtuk.java
@@ -0,0 +1,13 @@
+public class Uchburchtuk {
+
+    int a;
+    int b;
+    int c;
+
+
+     void method (){
+         double p= (a+b+c)/2;
+         System.out.println("Uchburchtun aianti "+(int)Math.sqrt(p*(p-a)*(p-b)*(p-c)));
+
+     }
+}
